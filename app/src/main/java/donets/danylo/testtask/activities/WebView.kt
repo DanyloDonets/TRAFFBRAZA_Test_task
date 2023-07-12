7f25d1442a455333778cac6536552d7c69a03673
Daniil_plt <91540920+luciddem1@users.noreply.github.com>
2023-07-13 02:07:51 +0300

    Nougts and crosses(+), webview(+), onesygnal(?), appsFlyer(+), asyncLogic(+), no design

--- app/src/main/java/donets/danylo/testtask/activities/WebView.kt
+++ app/src/main/java/donets/danylo/testtask/activities/WebView.kt
@@ -1,12 +1,20 @@
 package donets.danylo.testtask.activities
 
 
+    import android.app.DownloadManager
     import android.content.Context
     import android.content.Intent
+    import android.content.pm.ActivityInfo
+    import android.net.Uri
     import android.os.Bundle
+    import android.os.Environment
     import android.util.Log
+    import android.view.WindowManager
+    import android.webkit.CookieManager
+    import android.webkit.URLUtil
     import android.webkit.WebView
     import android.webkit.WebViewClient
+    import android.widget.Toast
     import androidx.appcompat.app.AppCompatActivity
     import com.appsflyer.AppsFlyerLib
     import donets.danylo.testtask.R
@@ -22,6 +30,9 @@ class WebView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
         webView = findViewById(R.id.webView)
         webView.webViewClient = MyWebViewClient()
         webView.settings.javaScriptEnabled = true
@@ -33,7 +44,24 @@ class WebView : AppCompatActivity() {
         val savedUrl = sharedPreferences.getString("lastUrl", initialUrl)
         webView.loadUrl(savedUrl!!)
 
-        // Додати кнопку "Назад" до панелі дій
+        webView.setDownloadListener({ url, userAgent, contentDisposition, mimeType, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimeType)
+            request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalFilesDir(this@WebView, Environment.DIRECTORY_DOWNLOADS, ".png")
+            val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+        })
+
+
+
+    // Додати кнопку "Назад" до панелі дій
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
