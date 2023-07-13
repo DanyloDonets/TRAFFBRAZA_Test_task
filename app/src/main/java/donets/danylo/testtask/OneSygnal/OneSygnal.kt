ff19369095e1c61e0bb9d6dcb210faee83a81f9b
Daniil_plt <91540920+luciddem1@users.noreply.github.com>
2023-07-13 21:44:33 +0300

    all done, no design

--- app/src/main/java/donets/danylo/testtask/OneSygnal/OneSygnal.kt
+++ app/src/main/java/donets/danylo/testtask/OneSygnal/OneSygnal.kt
@@ -5,25 +5,38 @@ import com.onesignal.OSNotificationOpenedResult
 import com.onesignal.OneSignal
 
 import android.app.Application
+import android.content.Intent
+import donets.danylo.testtask.activities.WebView
 
 
 class OneSignal(private val context: Context) {
 
+    private val ONESIGNAL_APP_ID = "5423c5d7-aef6-4f49-b801-d61b042fe7cb"
+
     fun init() {
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
         OneSignal.initWithContext(context)
-        OneSignal.setAppId("OWFlODJmNDAtMTcwNS00OTc4LWEzMjMtZWM2MjljYjU2YWVk")
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.NONE)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications()
+
         OneSignal.setNotificationOpenedHandler(MyNotificationOpenedHandler())
     }
 
     private inner class MyNotificationOpenedHandler : OneSignal.OSNotificationOpenedHandler {
         override fun notificationOpened(result: OSNotificationOpenedResult) {
-            val notification = result.notification
-            val data = notification.additionalData
-
-            // Обробка відкриття повідомлення OneSignal
-            // ...
-
+            val intent = Intent(context, WebView::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
         }
     }
 }
+
+
+
+
