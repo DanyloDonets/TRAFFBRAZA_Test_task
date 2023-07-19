2c4492667f939a0077be8d818e16f127f9bf8099
Daniil_plt <91540920+luciddem1@users.noreply.github.com>
2023-07-19 11:34:10 +0300

    Uploading files in WebView

--- app/src/main/java/donets/danylo/testtask/activities/MainActivity.kt
+++ app/src/main/java/donets/danylo/testtask/activities/MainActivity.kt
@@ -1,14 +1,18 @@
 package donets.danylo.testtask.activities
 
 
+import android.Manifest
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import donets.danylo.testtask.R
 import kotlin.system.exitProcess
 
@@ -24,8 +28,7 @@ class MainActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
 
-
-
+        checkPermissions()
 
         gameBtn = findViewById(R.id.gameBtn)
         webBtn = findViewById(R.id.webBtn)
@@ -69,6 +72,58 @@ class MainActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+
+    private val READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 1
+    private val WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 2
+
+    // Функція для перевірки дозволів
+    private fun checkPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        val permissionsToRequest = mutableListOf<String>()
+
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission)
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), READ_EXTERNAL_STORAGE_PERMISSION_REQUEST)
+        }
+    }
+
+    // Обробник результатів запиту дозволів
+    // Обробник результатів запиту дозволів
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_PERMISSION_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Дозвіл на доступ до читання зовнішнього сховища отримано
+                    // Виконувати необхідні дії
+                } else {
+                    // Дозвіл на доступ до читання зовнішнього сховища не отримано
+                    // Обробка відсутності дозволу
+                }
+            }
+            WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Дозвіл на доступ до запису в зовнішнє сховище отримано
+                    // Виконувати необхідні дії
+                } else {
+                    // Дозвіл на доступ до запису в зовнішнє сховище не отримано
+                    // Обробка відсутності дозволу
+                }
+            }
+        }
+    }
+
 
 
 }
--- app/src/main/java/donets/danylo/testtask/activities/WebView.kt
+++ app/src/main/java/donets/danylo/testtask/activities/WebView.kt
@@ -1,30 +1,38 @@
 package donets.danylo.testtask.activities
 
-
-    import android.app.DownloadManager
-    import android.content.Context
-    import android.content.Intent
-    import android.content.pm.ActivityInfo
-    import android.net.Uri
-    import android.os.Bundle
-    import android.os.Environment
-    import android.util.Log
-    import android.view.WindowManager
-    import android.webkit.CookieManager
-    import android.webkit.URLUtil
-    import android.webkit.WebView
-    import android.webkit.WebViewClient
-    import android.widget.Toast
-    import androidx.appcompat.app.AppCompatActivity
-    import com.appsflyer.AppsFlyerLib
-    import donets.danylo.testtask.R
-
+import android.Manifest
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.WindowManager
+import android.webkit.CookieManager
+import android.webkit.URLUtil
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import donets.danylo.testtask.R
 
 class WebView : AppCompatActivity() {
+
     private lateinit var webView: WebView
-    private var initialUrl = "https://www.google.com/"
-    private var mainActivityIntent: Intent? = null
-    private var isFirstPageLoaded = false
+    private val initialUrl = "https://www.google.com/"
+
+
+    private val fileChooserRequestCode = 1
+    private var fileChooserCallback: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,15 +49,12 @@ class WebView : AppCompatActivity() {
         webView.settings.allowContentAccess = true
         webView.settings.allowFileAccess = true
 
-        mainActivityIntent = Intent(this, MainActivity::class.java)
-
         val sharedPreferences = getSharedPreferences("WebViewHistoryPrefs", Context.MODE_PRIVATE)
         val savedUrl = sharedPreferences.getString("lastUrl", initialUrl)
         webView.loadUrl(savedUrl!!)
 
-        webView.setDownloadListener({ url, userAgent, contentDisposition, mimeType, contentLength ->
+        webView.setDownloadListener { url, userAgent, contentDisposition, mimeType, _ ->
             val request = DownloadManager.Request(Uri.parse(url))
-
             request.setMimeType(mimeType)
             request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
             request.addRequestHeader("User-Agent", userAgent)
@@ -57,66 +62,101 @@ class WebView : AppCompatActivity() {
             request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
             request.allowScanningByMediaScanner()
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            request.setDestinationInExternalFilesDir(this@WebView, Environment.DIRECTORY_DOWNLOADS, ".png")
+            request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, ".png")
             val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
             dm.enqueue(request)
             Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
-        })
+        }
 
-
-
-
-
-    // Додати кнопку "Назад" до панелі дій
+        // Add "Up" button to the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onBackPressed() {
 
 
-        if (webView.url == initialUrl) {
-
-
-            val intent = Intent(this@WebView, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        else if (webView.canGoBack()) {
-            webView.goBack()
-        }
-
-             else {
-                webView.loadUrl(initialUrl)
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                fileChooserCallback = filePathCallback
+                val intent = fileChooserParams?.createIntent()
+                try {
+                    if (intent != null) {
+                        startActivityForResult(intent, fileChooserRequestCode)
+                    }
+                } catch (e: ActivityNotFoundException) {
+                    fileChooserCallback = null
+                    Toast.makeText(
+                        this@WebView,
+                        "File picker not found",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return false
+                }
+                return true
             }
         }
 
 
 
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == fileChooserRequestCode) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                if (fileChooserCallback != null) {
+                    val result = if (data.data != null) arrayOf(data.data!!) else null
+                    fileChooserCallback?.onReceiveValue(result)
+                }
+                fileChooserCallback = null
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
+
+
+
+
     private inner class MyWebViewClient : WebViewClient() {
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
             title = view?.title
+
             val sharedPreferences = getSharedPreferences("WebViewHistoryPrefs", Context.MODE_PRIVATE)
             val editor = sharedPreferences.edit()
             editor.putString("lastUrl", webView.url)
             editor.apply()
 
-            if (url != null) {
-                if (url.contains("fex.net")) {
-                    Log.i("RD_", "send")
-                    AppsFlyerLib.getInstance().logEvent(applicationContext, "fexnet_event", null)
-                }
-            }
-            if (url == initialUrl) {
-                isFirstPageLoaded = true
+            if (url != null && url.contains("fex.net")) {
+                Log.i("WebViewActivity", "Send AppsFlyer event")
+                // Perform AppsFlyer event logging here
             }
         }
     }
-}
 
+    override fun onDestroy() {
+        super.onDestroy()
+        // Clear WebView cache and history
+        webView.clearCache(true)
+        webView.clearHistory()
+    }
+
+
+}
